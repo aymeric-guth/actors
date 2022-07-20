734789860097c0a468d1e57cf5adbf681ff56aaa
Aymeric Guth <aymeric.guth@protonmail.com>
2022-07-20 22:12:07 +0200

    added explicits exports

--- actors/__init__.py
+++ actors/__init__.py
@@ -5,3 +5,25 @@ from .actor_system import actor_system, create, forward, ActorSystem, send
 from .base_actor import ActorGeneric
 from .errors import ActorException, DispatchError, ActorNotFound, SystemMessage
 from ._send import Send
+
+__all__ = [
+    "Sig",
+    "Message",
+    "MsgCtx",
+    "Event",
+    "Request",
+    "Response",
+    "Actor",
+    "ActorIO",
+    "actor_system",
+    "create",
+    "forward",
+    "ActorSystem",
+    "send",
+    "ActorGeneric",
+    "ActorException",
+    "DispatchError",
+    "ActorNotFound",
+    "SystemMessage",
+    "Send",
+]
